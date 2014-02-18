89442a3230624d8a100f77e5135d1bc66b3d905c
cjnavarro <cjnavarrowpi@gmail.com>
2014-02-18 16:32:56 -0500

    Update linkedlist.cpp

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -1,10 +1,4 @@
-/* 
-Author: Bob Kinicki
-
-Member functions for linked list implementation
-based Langsam et al textbook.      
-
-Note - very concise for loops came from Langsam  */
+/*Note - very concise for loops came from Langsam  */
 
 #include "List.h"
 #include <stdio.h>
@@ -12,8 +6,6 @@ Note - very concise for loops came from Langsam  */
 #include <iostream>
 #include <string>
 
-using std::cout;
-using std::endl;
 using namespace std;
 
 /* List is a default constructor that initializes a newly
@@ -23,17 +15,4 @@ List::List() {
   listptr = NULL;
 }
 
-//~List is the destructor that traverses the nodes of a
-// list, freeing them one by one.
 
-List::~List() {
-  // Use standard two pointer scheme to run down the list
-  // deleting list nodes.
-  ListPtr p, q;
-  p = listptr;
-  while (p != NULL) {
-    q = p->next;
-    delete p;
-    p = q;
-  }
-}
