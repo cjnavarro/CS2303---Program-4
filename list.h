106bd03e9053248a4696514e9fd5993ad2cec7db
Hoangngo-dev <Hoangngo-dev@users.noreply.github.com>
2014-02-21 16:57:35 -0500

    Update list.h

--- list.h
+++ list.h
@@ -1,27 +1,49 @@
-
+/*
+ * File: list.h
+ * -------------
+ * Header of list.cpp
+ */
+ 
 #include <string>
 
 using namespace std;
 
 class List {
+public:
+	/*
+	 * Constructs an empty list
+	 */
+  List ();
+  
+  /*
+   * Inserts an new word in to a list OR
+   * Increments the counter of the word and appends a line number
+   */
+  void insert(string word, int lineNum);
+  
+  /*
+   * Prints out the contents of the list
+   */
+  void print_list();
+  
+  /*
+   * Returns true if a word is not in the list
+   */
+  bool not_in_list(string word);
 
-//Self referential linked-list structure
+
+/* Private section */
+
 private:
   struct lnode {
     string word;
-	int count;
-	string lineCount;
+		int count;
+		string lineCount;
     struct lnode *next;
   };
+  
   typedef struct lnode *ListPtr;
 
   ListPtr listPtr;
-
-public:
-  // prototypes for member functions
-  List ();
-  void insert(string word, int lineNum);
-  void print_list();
-  bool not_in_list(string word);
 }; 
 
